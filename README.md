354e2f7607489e1b1b74b3b57c8d1f1a97993893
sittingbull <pr@little-big-data.com>
2017-02-21 20:10:57 +0100

    Addded Perf de l'IA in results + WIP Anim

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Kour
-
+Notes pour VL
+--
+Bien préciser :
+L'IA ne connait pas les règles
+L'IA ne peut pas tout tester : en effet il y a 10^50 possibilités
+Attention : les gens peuvent dire c'est pas de l'IA c'est du gros calcul
 Backlog
 --
 Done
@@ -7,31 +12,35 @@ Done
  apparition des flèches de conversion en plus des flèches normales 
 Doing
 --
-### Les KPIs
+
 OK empty conv - rectange avec 1000 / 100% lors de "lancer camp."
 OK calcul selon valeur des noeuds
 OK calcul via matrice: perf message / canal * transition * aud
 OK ajout rectangles finaux
 OK Résultats CAC / Reach / Dépense média sur un run
-
+(0.5) Animation campagne : chiffres défilants + "retry" (on click, hide camp. & allow modif)
+(0.2) Fixer le bug du "collapse"
+(2) IA : Calcul du résultat optimisé sur conversions / cac
+(1) IA : multiples essais randoms sur le parcours défini avant résultat
 
 Doable
 --
 ### Sûr
-(0.5) Animation campagne : chiffres défilants + "retry" (on click, hide camp. & allow modif)
-(0.2) Fixer le bug du "collapse"
 
-(2) IA : Calcul du résultat optimisé sur conv -- à partir de l'arbre existant
-(1) IA : multiples essais randoms sur le parcours défini avant résultat
+(0.5) Afficher un "coût de convergence" = coût de l'IA pour converger = distance d'arbre * conversions
 (0.5) Légende : "parcours sur le personna image + caractéristiquee" + pictos des créas humour & co.
 Bouton droit : relancer; expliquer les eurs / like / neg / unreached
 
+(0.5) Pour le négatif, splitter entre "no reaction" et "matching issue"
 
-### TBD
-1. (2) Résultats en graphes suivis avec le temps
+(0.5) le graphe se reconstruit quand le parcours grandit
 
-2. Pour le négatif, splitter entre "no reaction" et "matching issue"
+(0.1) alerte if refresh / close
 
-3. (opt) Afficher un "coût de convergence" = coût de l'IA pour converger = distance d'arbre * conversions
+(0.1) mettre à dispo le truc online
 
-3.opt (2) Plusieurs profils 
+(2) Résultats en graphes suivis avec le temps
+
+
+
+
--- kour.js
+++ kour.js
@@ -1,3 +1,6 @@
+// This variable's null value is used as a flag indicating that "OptimizeByIA" has
+// not been clicked
+var resultsWithoutIA = null; 
 
 function launchCampaign() {
     // Change new nodes into "end" nodes and add conversion boxes for end nodes
@@ -28,44 +31,32 @@ function launchCampaign() {
 
     // Display results
     var results = getCampaignResults();
+
     d3.select('.results').attr("style", "display: block;");
     ['media-cost', 'cac', 'conversions'].forEach(function(kpi) {
 	animateToNumber(d3.select('.results .'+kpi), function(d) { return results[kpi]; });
     });
-
-    d3.select('button.launch').attr("disabled",true);
-}
-
-function retry() {
-    var midpaths = d3.selectAll("g.midpath");
-    midpaths.select('g').attr("style","display: none;");
-    d3.select('.results').attr("style", "display: none;");
-    midpaths.select('image').attr("style","display: block;");
-
-    var treeData = treemap(root);
-    var nodes = treeData.descendants();
-    nodes.forEach(function(node) {
-	if (node.data.channel == "stop") {
-	    node.data.channel = "new";
-	    node.data.name = NOUV_ETAPE;
-	} else if (node.children && node.children.length == 1 && node.children[0].data.type == "conversion") {
-	    node.children = null; 
-	}
-    });
     
-    update(root);
-    d3.select('button.launch').attr("disabled", null);
+    if (resultsWithoutIA) {
+	var iaPerf = getIAPerf(results);
+	d3.select('.results-ia').attr("style", "display: block;");
+	['learning-time', 'conversion-gain', 'cac-gain'].forEach(function(kpi) {
+	    animateToNumber(d3.select('.results-ia .'+kpi), function(d) { return iaPerf[kpi]; });
+	});
+    }
+    
+    d3.select('button.launch').attr("disabled",true);
+    d3.select('button.optim').attr("disabled",true);
+    return results;
 }
 
-function animateToNumber(selection, transfNumber) {
-    return selection.transition().duration(1500)
-	    .tween("text", function(d) {
-		var that = d3.select(this),
-		    i = d3.interpolateNumber(0, transfNumber(d));
-		return function(t) {
-		    that.text(formatNumber(i(t)));
-		};
-	    });
+function getIAPerf(resultsWithIA) {
+    var learningTime = Math.floor(- 10 * Math.log((resultsWithoutIA["conversions"]/resultsWithoutIA["cac"])/(resultsWithIA["conversions"]/resultsWithIA["cac"])));
+    return {
+	"learning-time": learningTime,
+	"conversion-gain": resultsWithIA["conversions"] - resultsWithoutIA["conversions"],
+	"cac-gain": 100*resultsWithIA["cac"]/resultsWithoutIA["cac"]
+    };
 }
 
 function getCampaignResults() {
@@ -87,9 +78,38 @@ function getCampaignResults() {
     };
 }
 
-var optimum = [{}, {}, {}, {}, {}];
-function optimizeByIA() {
+function retry() {
+    var midpaths = d3.selectAll("g.midpath");
+    midpaths.select('g').attr("style","display: none;");
+    d3.select('.results').attr("style", "display: none;");
+    midpaths.select('image').attr("style","display: block;");
+    d3.select('.results-ia').attr("style", "display: none;");
+    var treeData = treemap(root);
+    var nodes = treeData.descendants();
+    nodes.forEach(function(node) {
+	if (node.data.channel == "stop") {
+	    node.data.channel = "new";
+	    node.data.name = NOUV_ETAPE;
+	} else if (node.children && node.children.length == 1 && node.children[0].data.type == "conversion") {
+	    node.children = null; 
+	}
+    });
+    
+    update(root);
+    d3.select('button.launch').attr("disabled", null);
+    d3.select('button.optim').attr("disabled", null);
+}
 
+
+var optimum = [{}, {}, {}, {}, {}];
+
+
+function optimizeByIA() {
+    if (!confirm("Lancer l'IA va modifier votre parcours actuel, êtes vous sûr?")) return false;
+    
+    // Save campaign results without IA
+    resultsWithoutIA = launchCampaign();
+    retry();
     // iterate on depth, node type, parent channel, parent message
     for(var depth=4; depth >=0 ; depth--) {
 	branches.forEach(function(nodeType) { branches.forEach(function(parentType) {
@@ -151,37 +171,52 @@ function optimizeByIA() {
     
     function _optimalStep(node, line) {
 	/* Sets the optimal message for node N with previous steps stored in line */
-	animateSubtree(node, Math.floor(Math.random() * 3) + 2, function() {
-	    optimum[node.depth][line].name = optimum[node.depth][line].message;
-	    node._updateStepData(optimum[node.depth][line]);
-	    update(node);
-	    if (node.children) {
-		for (var chInd = 0; chInd < node.children.length; chInd++) {
-		    var child = node.children[chInd];
-		    _optimalStep(child, optimum[node.depth][line].nextOptimum + child.data.type);
-		}
-	    }
+	return new Promise(function(resolve) {
+	    animateSubtree(node, Math.floor(Math.random() * 3) + 2).then(function() {
+		optimum[node.depth][line].name = optimum[node.depth][line].message;
+		node._updateStepData(optimum[node.depth][line]);
+		update(node);
+		if (node.children) {
+		    for (var chInd = 0; chInd < node.children.length; chInd++) {
+			var child = node.children[chInd];
+			_optimalStep(child, optimum[node.depth][line].nextOptimum + child.data.type).then(() => resolve());
+		    }
+		} else { resolve();}
+	    });
 	});
     }
 
 
-    function animateSubtree(node, iterations, afterAnimation) {
-	if (iterations == 0) { afterAnimation();}
-	else {
-	    node.descendants().concat(node).forEach(function(nd) {
-		nd.data.channel = channels[Math.floor(Math.random()*channels.length)];
-		nd.data.message = messages[Math.floor(Math.random()*messages.length)];
-		nd.data.name = nd.data.message;
-	    });
-	    update(node);
-	    sleep(100).then(function() { animateSubtree(node, iterations - 1, afterAnimation); })
-	}
+    function animateSubtree(node, iterations) {
+	return new Promise(function(resolve, reject) {
+	    if (iterations == 0) resolve();
+	    else {
+		node.descendants().concat(node).forEach(function(nd) {
+		    nd.data.channel = channels[Math.floor(Math.random()*channels.length)];
+		    nd.data.message = messages[Math.floor(Math.random()*messages.length)];
+		    nd.data.name = nd.data.message;
+		});
+		update(node);
+		sleep(100).then(function() { animateSubtree(node, iterations - 1).then(() => resolve()) })
+	    }
+	});
     }
     // initial line
-    _optimalStep(root, messages[0] + channels[0] + branches[0] + messages[0] + channels[0] + branches[1]);
-    
+    _optimalStep(root, messages[0] + channels[0] + branches[0] + messages[0] + channels[0] + branches[1])
+	.then(function() { console.log('Launching'); launchCampaign() });
 }
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function animateToNumber(selection, transfNumber) {
+    return selection.transition().duration(1500)
+	    .tween("text", function(d) {
+		var that = d3.select(this),
+		    i = d3.interpolateNumber(0, transfNumber(d));
+		return function(t) {
+		    that.text(formatNumber(i(t)));
+		};
+	    });
 }
